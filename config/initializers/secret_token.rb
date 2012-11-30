1de3e37d951927c69ac38c87b24785434ce01ae2
regiseduardogregol <regiseduardogregol@gmail.com>
2012-11-30 11:07:26 -0200

    begin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webkvm::Application.config.secret_token = '60d4c0b7fe470e446e919b84b5c0b61abafe799da7107cd79e2a415d48c5189ddbc1d5a0d05dedf086df261ff027ed4aee42f8f7c80328dd70dd76ddf009c608'
